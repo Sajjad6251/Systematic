dce250fc2492f496dce5fd5148d243295084ec9a
Sabbir Hossan Sajjad <96525968+Sajjad6251@users.noreply.github.com>
2021-12-30 14:19:03 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Bx Slider
+	$(document).ready(function(){
+		$('.slider_area').bxSlider();
+	});
+  
+  // Meanmenu
+	jQuery(document).ready(function () {
+		jQuery('header nav').meanmenu();
+	});
